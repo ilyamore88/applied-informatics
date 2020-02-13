2cc78605ed00862d6b5d6af42c913be82503e289
ilyamore88 <themorozik@gmail.com>
2020-02-13 11:22:40 +0300

    add reading and writing matrices, build project

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,7 +6,70 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Enter size of matrix
+             */
+            Console.Write("Enter n(size of matrix): ");
+            int n = Int32.Parse(Console.ReadLine());
+            int[,] matrix1 = new int[n, n];
+            int[,] matrix2 = new int[n, n];
+
+            /*
+             * Enter matrix #1
+             */
+            Console.WriteLine("Enter matrix #1:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix1[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            /*
+             * Enter matrix #2
+             */
+            Console.WriteLine("Enter matrix #2:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix2[i, j] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            /*
+             * Clear console
+             */
+            Console.Clear();
+
+            /*
+             * Write matrix #1 to console
+             */
+            Console.WriteLine("Matrix #1:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix1[i, j].ToString() + '\t');
+                }
+
+                Console.WriteLine();
+            }
+
+            /*
+             * Write matrix #2 to console
+             */
+            Console.WriteLine("Matrix #2:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix2[i, j].ToString() + '\t');
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }

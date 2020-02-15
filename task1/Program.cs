4eb4d134d9c9851020a2b607f00e4f5ae7a470b5
ilyamore88 <themorozik@gmail.com>
2020-02-15 22:23:58 +0300

    add method for printing matrices

--- task1/Program.cs
+++ task1/Program.cs
@@ -47,29 +47,13 @@ namespace task1
              * Write matrix #1 to console
              */
             Console.WriteLine("Matrix #1:");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(matrix1[i, j].ToString() + '\t');
-                }
-
-                Console.WriteLine();
-            }
+            PrintMatrix(matrix1);
 
             /*
              * Write matrix #2 to console
              */
             Console.WriteLine("Matrix #2:");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(matrix2[i, j].ToString() + '\t');
-                }
-
-                Console.WriteLine();
-            }
+            PrintMatrix(matrix2);
 
             /*
              * Matrix sum
@@ -92,6 +76,22 @@ namespace task1
                 for (int j = 0; j < n; j++)
                 {
                     Console.Write(matrixSum[i, j].ToString() + '\t');
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        /**
+         * Print matrix in console
+         */
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j].ToString() + '\t');
                 }
 
                 Console.WriteLine();

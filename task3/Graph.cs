37b87daffd41119c0c57770ad414f4f6a4ad9d91
ilyamore88 <themorozik@gmail.com>
2020-03-04 18:13:26 +0300

    realize depth first search

---
+++ task3/Graph.cs
@@ -0,0 +1,15 @@
+﻿namespace task3
+{
+    public abstract class Graph
+    {
+        /*
+         * Return count of vortexes
+         */
+        public abstract int VortexCount();
+
+        /*
+         * Return array of vortex neighbours
+         */
+        public abstract int[] GetVortexNeighbours(int vortex);
+    }
+}
